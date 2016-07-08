f041bbf72f4a8e832d659ace51bfd9e8405e39f4
Jean Joeris <joeris.peter@gmail.com>
2016-07-08 08:49:34 -0600

    cleaned up CompleteMe

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -5,15 +5,3 @@ require '/Users/jean/turing/1module/project/complete_me/lib/dictionary_tree'
 class CompleteMe < DictionaryTree
 
 end
-
-# kept around for proving traverse counting words
-# does work. @count is more performant, but could be right while the tree is not
-#
-# def count(node = @root)
-#
-#   node.children.map do |child_node|
-#     @word_count +=1 if child_node.is_word
-#     count(child_node)
-#   end
-#   @word_count
-# end
